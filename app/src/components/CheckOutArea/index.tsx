df4ffc9044b752d1baa7bd3964dcf3659e901a1e
ferreira-joao <klausblack231@gmail.com>
2022-04-19 12:38:51 -0300

    created checkout function

--- app/src/components/CheckOutArea/index.tsx
+++ app/src/components/CheckOutArea/index.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Alert } from "react-native";
 
 import { Container, Total, CheckButton, CheckButtonText } from "./styles";
+
+import { ShoppingCartContext } from "../../contexts/ShoppingCartContext";
 
 interface ITotal {
   total: number;
 }
 
 export function CheckOutArea({ total }: ITotal) {
+  const [cart, setCart] = useContext(ShoppingCartContext);
+
+  function handleCheckout() {
+    Alert.alert("Thanks for the purchase!");
+    setCart([]);
+  }
+
   return (
     <Container>
       <Total>$ {total}</Total>
 
-      <CheckButton>
+      <CheckButton onPress={handleCheckout}>
         <CheckButtonText>Check out</CheckButtonText>
       </CheckButton>
     </Container>
